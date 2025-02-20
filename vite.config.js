01efc6d109a865bc4fb9673c468777ca4fbdc9c0
emigreif <emilianogreif@gmail.com>
2025-02-19 23:22:54 -0300

    actualice

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './',  // Asegura que las rutas sean relativas
+  base: './', // Asegura que las rutas sean relativas para producción
   build: {
     outDir: 'dist',
     emptyOutDir: true,
