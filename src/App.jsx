87dbf5607cafd72bc583b28a0539512805064129
emigreif <emilianogreif@gmail.com>
2025-02-25 20:00:33 -0300

    ewr

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import ObrasList from "./pages/ObrasList.jsx";
 import ObraDetail from "./pages/ObraDetail.jsx";
 import ProveedoresList from "./pages/ProveedoresList.jsx";
 import MapaObras from "./components/MapaObras.jsx";
-import "leaflet/dist/leaflet.css";
 import Panol from "./pages/Panol.jsx";
 import Calendario from "./pages/Calendario.jsx";
 import Admin from "./pages/Admin.jsx";
--- src/components/MapaObras.jsx
+++ src/components/MapaObras.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import "leaflet/dist/leaflet.css";
+
 import L from "leaflet";
 
 // Definir icono personalizado para el marcador
