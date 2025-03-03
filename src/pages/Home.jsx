fc043e9b203c889ada615b904bc087d14277f32f
emigreif <emilianogreif@gmail.com>
2025-03-03 18:21:58 -0300

    s

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,15 @@
-import { Link } from 'react-router-dom';
-import '../styles/Home.css';
+import { Link } from "react-router-dom";
+import "../styles/Home.css";
 
 const Home = () => {
   return (
     <div className="home-container">
-      {/* Sección Izquierda (Blanca) */}
       <div className="home-left">
         <div className="home-text">
           <h1 className="planner-title">PLANNER</h1>
-          <h3 className="griff-subtitle">Greif Software Solutions</h3>
+          <h3 className="greif-subtitle">Greif Software Solutions</h3>
           <p className="home-description">
-            Organiza tus tareas y proyectos de forma sencilla y eficiente. 
+            Organiza tus tareas y proyectos de forma sencilla y eficiente.
             ¡Bienvenido/a a tu nuevo espacio de productividad!
           </p>
           <Link to="/login" className="home-button">
@@ -18,12 +17,7 @@ const Home = () => {
           </Link>
         </div>
       </div>
-
-      {/* Sección Derecha (Verde / Ilustración) */}
-      <div className="home-right">
-        {/* Opcional: imagen o SVG decorativo */}
-        {/* <img src="/assets/imagen-ilustracion.svg" alt="Ilustración" /> */}
-      </div>
+      <div className="home-right"></div>
     </div>
   );
 };
