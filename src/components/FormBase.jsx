91cfaa7e584bf6f18ef6f4e6adac1e165101ee8b
emigreif <157245016+emigreif@users.noreply.github.com>
2025-03-08 15:23:43 -0300

    Crear FormBase.jsx

---
+++ src/components/FormBase.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import "./FormBase.css";
+
+const FormBase = ({ fields, onSubmit, onCancel }) => {
+  return (
+    <form className="form-base" onSubmit={onSubmit}>
+      {fields.map((field, index) => (
+        <div key={index} className="form-group">
+          <label>{field.label}:</label>
+          <input
+            type={field.type || "text"}
+            name={field.name}
+            value={field.value}
+            onChange={field.onChange}
+            required={field.required || false}
+          />
+        </div>
+      ))}
+      <div className="form-actions">
+        <button type="submit">Guardar</button>
+        <button type="button" onClick={onCancel}>Cancelar</button>
+      </div>
+    </form>
+  );
+};
+
+export default FormBase;
